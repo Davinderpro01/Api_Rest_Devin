aec27f280274a6cb7296fff74ac613f759844160
Davinderpro01 <d-alex01@hotmail.com>
2022-08-27 16:18:25 -0600

    primer commit

---
+++ controllers/tracks.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getItems = (req, res) =>{
+    const data = ['hola','mundo']
+    res.send({data})
+};
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getItem = (req, res) =>{};
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
+const createItem = (req, res) =>{};
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
+const updateItem = (req, res) =>{};
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
+const deleteItem = (req, res) =>{};
+
+module.exports = {getItems, getItem, createItem, updateItem, deleteItem};
