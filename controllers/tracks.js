4cf0574d9a5236ac05b23e49b37dce6507edc1ad
Davinderpro01 <d-alex01@hotmail.com>
2022-09-04 22:10:41 -0600

    commit

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -1,13 +1,20 @@
 const {tracksModel} = require ('../models');
+const {matchedData} = require('express-validator');
+const { handleHttpError } = require('../utils/handleError');
 /**
  * 
  * @param {*} req 
  * @param {*} res 
  */
 const getItems = async (req, res) =>{
-    const data = await tracksModel.find({});
-
-    res.send({data});
+    
+    try{
+        const data = await tracksModel.find({});
+        res.send({data});
+    }catch(e){
+        handleHttpError(res, 'ERROR_GET_ITEMS');
+    }
+    
 };
 
 /**
@@ -15,18 +22,33 @@ const getItems = async (req, res) =>{
  * @param {*} req 
  * @param {*} res 
  */
-const getItem = (req, res) =>{};
+const getItem = async (req, res) =>{
+    try {
+        req = matchedData(req);
+        const {id} = req;
+        const data = await tracksModel.findById(id);
+        res.send({data});
+    } catch (e) {
+        handleHttpError(res, 'ERROR_GET_ITEM')
+    }
+};
 
 /**
  * 
  * @param {*} req 
  * @param {*} res 
  */
+
 const createItem = async (req, res) =>{
-    const {body} = req
-    console.log(body)
-    const data = await tracksModel.create(body)
-    res.send({data})
+
+    try{
+        const body = matchedData(req)
+        const data = await tracksModel.create(body);
+        res.send({data});
+    }catch(e){
+        handleHttpError(res, 'ERROR_CREATE_ITEMS');
+    }
+    
 };
 
 /**
@@ -34,13 +56,32 @@ const createItem = async (req, res) =>{
  * @param {*} req 
  * @param {*} res 
  */
-const updateItem = (req, res) =>{};
+const updateItem = async (req, res) =>{
+    try{
+        const {id, ...body} = matchedData(req);
+        const data = await tracksModel.findOneAndUpdate(
+            id, body
+        );
+        res.send({data});
+    }catch(e){
+        handleHttpError(res, 'ERROR_UPDATE_ITEMS');
+    }
+};
 
 /**
  * 
  * @param {*} req 
  * @param {*} res 
  */
-const deleteItem = (req, res) =>{};
+const deleteItem = async (req, res) =>{
+    try {
+        req = matchedData(req);
+        const {id} = req;
+        const data = await tracksModel.deleteOne({_id:id});
+        res.send({data});
+    } catch (e) {
+        handleHttpError(res, 'ERROR_DELETE_ITEM')
+    }
+};
 
 module.exports = {getItems, getItem, createItem, updateItem, deleteItem};
